3689e5e3a1289f713dffbb833a34e162d7775e83
BhargavP983 <bhargav09p03@gmail.com>
2023-01-26 02:42:56 +0530

    modified:   css/home.css 	modified:   css/login.css 	modified:   home.html 	new file:   js/events.js 	modified:   js/slider.js 	modified:   login.html

--- js/slider.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function(){
-    var parallax = document.querySelector(".parallax-background");
-    var scrolled = window.pageYOffset;
-    parallax.style.top = -(scrolled * 0.5) + "px";
-  });
-  
